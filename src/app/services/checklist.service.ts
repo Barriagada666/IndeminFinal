07410ac351a9ba5e78bc10c54f5ae63ea4a2a2c3
AlvaroFenero <al.fenero@duocuc.cl>
2024-07-12 22:28:22 -0400

    solucion

--- src/app/services/checklist.service.ts
+++ src/app/services/checklist.service.ts
@@ -8,7 +8,7 @@ import { Checklist } from '../models/Checklist';
   providedIn: 'root'
 })
 export class ChecklistService {
-  private baseUrl = 'https://backend-indemin-q64w.onrender.com';  
+  private baseUrl = 'https://backend-indemin-q64w.onrender.com/api';  
 
   supabaseHeaders = new HttpHeaders()
     .set('Content-Type', 'application/json')
